8ba1fba4e44106c715d5d8bb5a07c83c58d22faa
Daniel Perrelly <danperrelly@gmail.com>
2015-08-04 13:12:13 -0400

    done

--- bash.js
+++ bash.js
@@ -4,15 +4,24 @@ commands = require('./commands.js');
 //4
 //5
 process.stdout.write('prompt > ');
+var done = function(output){
+	process.stdout.write(output + '\n\nprompt > ');
+	if(commandList.length > 0) commands.input[commandList.shift()](output, null, done);
+};
 
+var commandList = [];
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
   data = data.toString().trim(); 
+
+  commandList = data.split(' | ');
+  commandList.shift();
+
   var cmd = data.split(' ')[0];// remove the newline
   
   var file = data.slice(data.indexOf(' ') + 1);
   
   if(cmd === 'date') process.stdout.write(Date());
-  commands.input[cmd](file);
+  commands.input[cmd](null, file, done);
 
 });
--- commands.js
+++ commands.js
@@ -1,38 +1,48 @@
 fs = require('fs');
+request = require('request');
+
 
 var commands = {
-	pwd: function (file){
-  	process.stdout.write(process.env.PWD);
-  		process.stdout.write('\nprompt > ');
+	pwd: function (file, done){
+  	done(process.env.PWD);
   	},
 
-  	ls: function(file){
+  	ls: function(file, done){
   		if(file == "ls") file = ".";
-  		console.log(file);
 	  	fs.readdir(file, function(err, files) {
 		  	if (err) throw err;
+		  	var output = "";
 		  	files.forEach(function(file) {
-		    	process.stdout.write(file.toString() + "\n");
+		    	output += file.toString() + "\n";
 		  	});
-	  		process.stdout.write("prompt >");
+		  	done(output.slice(0,-1));
 		});
   	},
-  	cat: function(file){
+  	cat: function(file, done){
   		var files = file.split(' ');
+		 
+
+		var output = "";
 
   		files.forEach(function(file){
 
-	  		fs.readFile(file, function(err, data){
-	  			if(err) throw err;
-	  			process.stdout.write(data);
-	  			//console.log();
-	  			process.stdout.write("\nprompt >");
-
-	  		});
+	  		output += (fs.readFileSync(file, 'utf8') + '\n\n');
+	  		
 
   		});
+  		done(output.slice(0,-1));
+
   	},
-  	head: function(file){
+  	head: function(stdin, file, done){
+  		console.log('HEAD');
+
+  		if(stdin){
+			
+			var topfive = stdin.match(/^((.*\n){0,4}(.*))\n?/)[1];
+	  		done(topfive);
+
+  		}else{
+
   		var files = file.split(' ');
 
   		fs.readFile(files[0], function(err, data){
@@ -40,32 +50,37 @@ var commands = {
 	  		data = data.toString();
 
 	  		var topfive = data.match(/^((.*\n){0,4}(.*))\n?/)[1];
-	  		process.stdout.write(topfive);
-	  			//console.log();
-	  		process.stdout.write("\nprompt >");
-
+	  		done(topfive);
 	  	});
 
+  		}
+
+  	},
+  	tail: function(stdin, file, done){
+  		console.log('TAIL');
+  		if(stdin){
+			
+			var bottomfive = stdin.match(/((.*\n){0,4}(.*))$/)[1];
+		  	done(bottomfive);
+
+  		}else{
+
+			var files = file.split(' ');
+
+	  		fs.readFile(files[0], function(err, data){
+		  		if(err) throw err;
+		  		data = data.toString();
+
+		  		var bottomfive = data.match(/((.*\n){0,4}(.*))$/)[1];
+		  		done(bottomfive);
+
+		  	});
+
+  		}
 
 
   	},
-  	tail: function(file){
-		var files = file.split(' ');
-
-  		fs.readFile(files[0], function(err, data){
-	  		if(err) throw err;
-	  		data = data.toString();
-
-	  		var topfive = data.match(/((.*\n){0,4}(.*))$/)[1];
-	  		process.stdout.write(topfive);
-	  		process.stdout.write("\nprompt >");
-
-	  	});
-
-
-
-  	},
-  	sort: function(file){
+  	sort: function(file, done){
 		
 		var files = file.split(' ');
 
@@ -73,13 +88,40 @@ var commands = {
 	  		if(err) throw err;
 	  		data = data.toString().split('\n');
 
-	  		//console.dir(data);
-
-	  		process.stdout.write(data.sort().join('\n'));
-	  		
-	  		process.stdout.write("\nprompt >");
+	  		done(data.sort().join('\n'));
 
 	  	});
+
+  	},
+
+  	uniq: function(file, done){
+
+		var files = file.split(' ');
+
+  		fs.readFile(files[0], function(err, data){
+	  		if(err) throw err;
+	  		data = data.toString().split('\n');
+
+
+	  		var result = [];
+	  		for(var i = 1; i < data.length; i++){
+	  			if(data[i] !== data[i-1]) result.push(data[i]);
+
+	  		}
+
+	  		done(result.join('\n'));
+
+	  	});  	
+  	},
+
+  	curl: function(file, done){
+  		var files = file.split(' ');
+
+  		request(files[0], function (error, response, body) {
+  		if (!error && response.statusCode == 200) {
+    		done(body); // Show the HTML for the Google homepage.
+  		}
+});
 
   	}
 };
